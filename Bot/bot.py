fe46b64a5b0d27e5c8ef9d0e5a1de6f5f2575a30
DHEERXJ2324 <70833570+DHEERXJ@users.noreply.github.com>
2022-05-20 23:32:48 +0530

    Add files via upload

---
+++ Bot/bot.py
@@ -0,0 +1,385 @@
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ( CommandHandler, Filters, MessageHandler, Updater)
+from message import Editmessage, Sendmessage, logger
+from Miscellaneous.Scraper import pastebin, text_scraper, throwbin, ghostbin
+import os
+import telepot
+import urllib.request
+from PIL import Image
+import requests
+from bs4 import BeautifulSoup
+import random
+import string
+import time
+import json
+import math
+dia='✅'
+
+os.environ['TZ'] = 'America/Buenos_Aires'
+
+
+bot_token = os.environ.get('TG_BOT_TOKEN')
+startmessage = [[
+		InlineKeyboardButton(
+			"Dev",
+			url='https://t.me/dheeraj2324'
+		),
+        InlineKeyboardButton(
+			"Channel",
+			url='https://t.me/aboutdheeraj'
+		)
+        ]]
+
+
+def start(update, context):
+    info = update.effective_user
+    print(info)
+    chat_id = info.id
+    print(chat_id)
+    userid= info['username']
+    text = f'Welcome @{userid}, to maths calculator bot and also private stuff dev has no relation to this, to know more use /help  This bot is provided for educational use!! , ENTER IN YOUR OWN RISK!!!!!!! . YOU ARE RESPONSIBLE FOR YOUR OWN ACTION!.'
+    Sendmessage(chat_id, text, reply_markup=InlineKeyboardMarkup(startmessage))
+    return
+def help(update, context):
+    chat_id = update.message.chat_id
+    text = "Available cmds available:\n /cal \n /add \n /sub \n /multiply \n /divide \n secrect cmds are onlY shared with known!!  ENTER IN YOUR OWN RISK!!!!!!!!"
+    Sendmessage(chat_id, text, reply_markup= InlineKeyboardMarkup(startmessage))
+def botcmds(update, context):
+    chat_id = update.message.chat_id
+    text = "Hey, welcome to this Bot! sorry to say cmds of the bots have been taken to private!!"
+    Sendmessage(chat_id, text)
+
+def ssc(update, context):
+    chat_id = update.message.chat_id
+    info = update.effective_user
+    userid= info['username']
+    text =  update.message.text.split(' ',1)
+    tempp=text[-1]
+    logger.info(text)
+    print(info)
+    textt=tempp.upper()
+    if textt=="21951A6626" or textt == "21951A6627" or textt == "21951A6637":
+        text = "Gods data not available"
+        Sendmessage(chat_id,text)
+    else:		
+        photos = "https://iare-data.s3.ap-south-1.amazonaws.com/uploads/STUDENTS/{}/DOCS/{}_SSC.jpg".format(textt,textt)
+        base_url = 'https://api.telegram.org/bot{}/'.format(bot_token)
+        payload = {
+            "chat_id" : chat_id,
+            "photo" : photos,
+            "caption" : "✅ Done!!"
+        }
+        to_url = 'https://api.telegram.org/bot{}/sendPhoto'.format(bot_token)
+        res=requests.post(to_url , data=payload)
+def fssc(update, context):
+    chat_id = update.message.chat_id
+    info = update.effective_user
+    userid= info['username']
+    text =  update.message.text.split(' ',1)
+    tempp=text[-1]
+    logger.info(text)
+    print(info)
+    textt=tempp.upper()
+    if textt=="21951A6626" or textt == "21951A6627" or textt == "21951A6637":
+        text = "Gods data not available"
+        Sendmessage(chat_id,text)
+    else:		
+        photos = "https://iare-data.s3.ap-south-1.amazonaws.com/uploads/IARE/STAFF/{}/DOC/{}_SSC.jpg".format(textt,textt)
+        base_url = 'https://api.telegram.org/bot{}/'.format(bot_token)
+        payload = {
+            "chat_id" : chat_id,
+            "photo" : photos,
+            "caption" : "✅ Done!!"
+        }
+        to_url = 'https://api.telegram.org/bot{}/sendPhoto'.format(bot_token)
+        res=requests.post(to_url , data=payload)
+def fpan(update, context):
+    chat_id = update.message.chat_id
+    info = update.effective_user
+    userid= info['username']
+    text =  update.message.text.split(' ',1)
+    tempp=text[-1]
+    logger.info(text)
+    print(info)
+    textt=tempp.upper()
+    if textt=="21951A6626" or textt == "21951A6627" or textt == "21951A6637":
+        text = "Gods data not available"
+        Sendmessage(chat_id,text)
+    else:		
+        photos = "https://iare-data.s3.ap-south-1.amazonaws.com/uploads/IARE/STAFF/{}/DOC/{}_PAN_CARD.jpg".format(textt,textt)
+        base_url = 'https://api.telegram.org/bot{}/'.format(bot_token)
+        payload = {
+            "chat_id" : chat_id,
+            "photo" : photos,
+            "caption" : "✅ Done!!"
+        }
+        to_url = 'https://api.telegram.org/bot{}/sendPhoto'.format(bot_token)
+        res=requests.post(to_url , data=payload)
+##################################################################################################################################################
+def inter(update, context):
+    chat_id = update.message.chat_id
+    info = update.effective_user
+    userid= info['username']
+    text =  update.message.text.split(' ',1)
+    tempp=text[-1]
+    print(info)
+    logger.info(text)
+    textt=tempp.upper()
+    if textt=="21951A6626" or textt == "21951A6627" or textt == "21951A6637":
+        text = "Gods data not available"
+        Sendmessage(chat_id,text)
+    else:		
+        photos = "https://iare-data.s3.ap-south-1.amazonaws.com/uploads/STUDENTS/{}/DOCS/{}_INTER.jpg".format(textt,textt)
+        base_url = 'https://api.telegram.org/bot{}/'.format(bot_token)
+        payload = {
+            "chat_id" : chat_id,
+            "photo" : photos,
+            "caption" : "✅ Done!!"
+        }
+        to_url = 'https://api.telegram.org/bot{}/sendPhoto'.format(bot_token)
+        res=requests.post(to_url , data=payload)
+def finter(update, context):
+    chat_id = update.message.chat_id
+    info = update.effective_user
+    userid= info['username']
+    text =  update.message.text.split(' ',1)
+    tempp=text[-1]
+    print(info)
+    logger.info(text)
+    textt=tempp.upper()
+    if textt=="21951A6626" or textt == "21951A6627" or textt == "21951A6637":
+        text = "Gods data not available"
+        Sendmessage(chat_id,text)
+    else:		
+        photos = "https://iare-data.s3.ap-south-1.amazonaws.com/uploads/IARE/STAFF/{}/DOC/{}_INTER.jpg".format(textt,textt)
+        base_url = 'https://api.telegram.org/bot{}/'.format(bot_token)
+        payload = {
+            "chat_id" : chat_id,
+            "photo" : photos,
+            "caption" : "✅ Done!!"
+        }
+        to_url = 'https://api.telegram.org/bot{}/sendPhoto'.format(bot_token)
+        res=requests.post(to_url , data=payload)
+def pic(update, context):
+    chat_id = update.message.chat_id
+    info = update.effective_user
+    userid= info['username']
+    text =  update.message.text.split(' ',1)
+    tempp=text[-1]
+    logger.info(text)
+    print(info)
+    textt=tempp.upper()
+    if textt=="21951A6626" or textt == "21951A6627" or textt == "21951A6637":
+        text = "Gods cann't be seen!!"
+        Sendmessage(chat_id,text)
+    else:		
+        photos = "https://iare-data.s3.ap-south-1.amazonaws.com/uploads/STUDENTS/{}/{}.jpg".format(textt,textt)
+        base_url = 'https://api.telegram.org/bot{}/'.format(bot_token)
+        payload = {
+            "chat_id" : chat_id,
+            "photo" : photos,
+            "caption" : "✅ Done!!"
+        }
+        to_url = 'https://api.telegram.org/bot{}/sendPhoto'.format(bot_token)
+        res=requests.post(to_url , data=payload)
+def fpic(update, context):
+    chat_id = update.message.chat_id
+    info = update.effective_user
+    userid= info['username']
+    text =  update.message.text.split(' ',1)
+    tempp=text[-1]
+    logger.info(text)
+    print(info)
+    textt=tempp.upper()
+    if textt=="21951A6626" or textt == "21951A6627" or textt == "21951A6637":
+        text = "Gods cann't be seen!!"
+        Sendmessage(chat_id,text)
+    else:		
+        photos = "https://iare-data.s3.ap-south-1.amazonaws.com/uploads/IARE/STAFF/{}/DOC/{}_PHOTO.jpg".format(textt,textt)
+        base_url = 'https://api.telegram.org/bot{}/'.format(bot_token)
+        payload = {
+            "chat_id" : chat_id,
+            "photo" : photos,
+            "caption" : "✅ Done!!"
+        }
+        to_url = 'https://api.telegram.org/bot{}/sendPhoto'.format(bot_token)
+        res=requests.post(to_url , data=payload)
+################################################################################################################################
+def bd(update, context):
+    chat_id = update.message.chat_id
+    info = update.effective_user
+    userid= info['username']
+    text =  update.message.text.split(' ',1)
+    tempp=text[-1]
+    logger.info(text)
+    print(info)
+    textt=tempp.upper()
+    if textt=="21951A6626" or textt == "21951A6627" or textt == "21951A6637":
+        text = "Gods data not available"
+        Sendmessage(chat_id,text)
+    else:		
+        photos = "https://iare-data.s3.ap-south-1.amazonaws.com/uploads/STUDENTS/{}/DOCS/{}_BIRTHCERTIFICATE.jpg".format(textt,textt)
+        base_url = 'https://api.telegram.org/bot{}/'.format(bot_token)
+        payload = {
+            "chat_id" : chat_id,
+            "photo" : photos,
+            "caption" : "✅ Done!!"
+        }
+        to_url = 'https://api.telegram.org/bot{}/sendPhoto'.format(bot_token)
+        res=requests.post(to_url , data=payload)
+################################################################################################################################################
+def aadhar(update, context):
+    chat_id = update.message.chat_id
+    info = update.effective_user
+    userid= info['username']
+    text =  update.message.text.split(' ',1)
+    tempp=text[-1]
+    logger.info(text)
+    print(info)
+    textt=tempp.upper()
+    if textt=="21951A6626" or textt == "21951A6627" or textt == "21951A6637":
+        text = "Gods data not available"
+        Sendmessage(chat_id,text)
+    else:		
+        photos = "https://iare-data.s3.ap-south-1.amazonaws.com/uploads/STUDENTS/{}/DOCS/{}_Aadhar.jpg".format(textt,textt)
+        base_url = 'https://api.telegram.org/bot{}/'.format(bot_token)
+        payload = {
+            "chat_id" : chat_id,
+            "photo" : photos,
+            "caption" : "✅ Done!!"
+        }
+        to_url = 'https://api.telegram.org/bot{}/sendPhoto'.format(bot_token)
+        res=requests.post(to_url , data=payload)
+	
+def all_details(update, context):
+    funcs = [pic, aadhar, ssc, inter, bd, eamcet, caste, income]
+    for i in funcs:
+        try: i(update, context)
+        except: pass
+	
+def faadhar(update, context):
+    chat_id = update.message.chat_id
+    info = update.effective_user
+    userid= info['username']
+    text =  update.message.text.split(' ',1)
+    tempp=text[-1]
+    logger.info(text)
+    print(info)
+    textt=tempp.upper()
+    if textt=="21951A6626" or textt == "21951A6627" or textt == "21951A6637":
+        text = "Gods data not available"
+        Sendmessage(chat_id,text)
+    else:		
+        photos = "https://iare-data.s3.ap-south-1.amazonaws.com/uploads/IARE/STAFF/{}/DOC/{}_AADHAR_CARD.jpg".format(textt,textt)
+        base_url = 'https://api.telegram.org/bot{}/'.format(bot_token)
+        payload = {
+            "chat_id" : chat_id,
+            "photo" : photos,
+            "caption" : "✅ Done!!"
+        }
+        to_url = 'https://api.telegram.org/bot{}/sendPhoto'.format(bot_token)
+        res=requests.post(to_url , data=payload)
+##########################################################################################################################################################
+def eamcet(update, context):
+    chat_id = update.message.chat_id
+    info = update.effective_user
+    userid= info['username']
+    text =  update.message.text.split(' ',1)
+    tempp=text[-1]
+    logger.info(text)
+    print(info)
+    textt=tempp.upper()
+    if textt=="21951A6626" or textt == "21951A6627" or textt == "21951A6637":
+        text = "Gods data not available"
+        Sendmessage(chat_id,text)
+    else:
+        photos = "https://iare-data.s3.ap-south-1.amazonaws.com/uploads/STUDENTS/{}/DOCS/{}_EAMCET_RANK.jpg".format(textt,textt)
+        base_url = 'https://api.telegram.org/bot{}/'.format(bot_token)
+        payload = {
+            "chat_id" : chat_id,
+            "photo" : photos,
+            "caption" : "✅ Done!!"
+        }
+        to_url = 'https://api.telegram.org/bot{}/sendPhoto'.format(bot_token)
+        res=requests.post(to_url , data=payload)
+def caste(update, context):
+    chat_id = update.message.chat_id
+    info = update.effective_user
+    userid= info['username']
+    text =  update.message.text.split(' ',1)
+    tempp=text[-1]
+    logger.info(text)
+    print(info)
+    textt=tempp.upper()
+    if textt=="21951A6626" or textt == "21951A6627" or textt == "21951A6637":
+        text = "Gods data not available"
+        Sendmessage(chat_id,text)
+    else:
+        photos = "https://iare-data.s3.ap-south-1.amazonaws.com/uploads/STUDENTS/{}/DOCS/{}_Caste.jpg".format(textt,textt)
+        base_url = 'https://api.telegram.org/bot{}/'.format(bot_token)
+        payload = {
+            "chat_id" : chat_id,
+            "photo" : photos,
+            "caption" : "✅ Done!!"
+        }
+        to_url = 'https://api.telegram.org/bot{}/sendPhoto'.format(bot_token)
+        res=requests.post(to_url , data=payload)
+def income(update, context):
+    chat_id = update.message.chat_id
+    info = update.effective_user
+    userid= info['username']
+    text =  update.message.text.split(' ',1)
+    tempp=text[-1]
+    logger.info(text)
+    print(info)
+    textt=tempp.upper()
+    if textt=="21951A6626" or textt == "21951A6627" or textt == "21951A6637":
+        text = "Gods data not available"
+        Sendmessage(chat_id,text)
+    else:
+        photos = "https://iare-data.s3.ap-south-1.amazonaws.com/uploads/STUDENTS/{}/DOCS/{}_Income.jpg".format(textt,textt)
+        base_url = 'https://api.telegram.org/bot{}/'.format(bot_token)
+        payload = {
+            "chat_id" : chat_id,
+            "photo" : photos,
+            "caption" : "✅ Done!!"
+        }
+        to_url = 'https://api.telegram.org/bot{}/sendPhoto'.format(bot_token)
+        res=requests.post(to_url , data=payload)
+#####################################################################################################################################################################
+
+
+def main():
+    updater = Updater(
+        bot_token,
+        use_context=True
+    )
+    dp = updater.dispatcher
+    #dp.add_handler(MessageHandler(Filters.text & ~Filters.command, duty))
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("pic", pic))
+    dp.add_handler(CommandHandler("Pic", pic))
+    dp.add_handler(CommandHandler("income", income))
+    dp.add_handler(CommandHandler("Income", income))
+    dp.add_handler(CommandHandler("caste", caste))
+    dp.add_handler(CommandHandler("Caste", caste))
+    dp.add_handler(CommandHandler("inter", inter))
+    dp.add_handler(CommandHandler("aadhar", aadhar))
+    dp.add_handler(CommandHandler("all", all_details))
+    dp.add_handler(CommandHandler("eamcet", eamcet))
+    dp.add_handler(CommandHandler("bd", bd))
+    dp.add_handler(CommandHandler("Aadhar", aadhar))
+    dp.add_handler(CommandHandler("botcmds", botcmds))
+    dp.add_handler(CommandHandler("ssc", ssc))
+    dp.add_handler(CommandHandler("fssc", fssc))
+    dp.add_handler(CommandHandler("finter", finter))
+    dp.add_handler(CommandHandler("fpan", fpan))
+    dp.add_handler(CommandHandler("fpic", fpic))
+    dp.add_handler(CommandHandler("faadhar", faadhar))
+    logger.info("Bot Started!!!")
+    updater.start_polling()
+    updater.idle()
+if __name__ == "__main__":
+    main()
