7e4dd2401769e595d07d95aefb133b739760f42c
DHEERXJ2324 <70833570+DHEERXJ@users.noreply.github.com>
2022-07-19 14:23:36 +0530

    Update message.py

--- Bot/message.py
+++ Bot/message.py
@@ -30,3 +30,8 @@ def sendfile(chat_id, file_id):
         bot.send_document(chat_id=chat_id, document = file_id)
     except Exception as e:
         logger.info(e)
+def Deletemessage(chat_id,msg_id):
+    try:
+        bot.delete_message(chat_id=chat_id , message_id = msg_id)
+    except Exception as e:
+        logger.info(e)
